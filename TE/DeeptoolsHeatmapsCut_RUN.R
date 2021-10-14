e00e8fe324443b5a3a382070c2c043a6b4273ace
gmoneyomics <agershm2@jhmi.edu>
2021-10-14 15:54:28 -0400

    updates for TE paper revision

--- TE/DeeptoolsHeatmapsCut_RUN.R
+++ TE/DeeptoolsHeatmapsCut_RUN.R
@@ -9,20 +9,24 @@ library(tidyverse)
 library(cowplot)
 library(BSgenome.t2t.v1.0.release)
 library(GenomicRanges)
+library(rtracklayer)
 library(zoo)
 # load meth 
 
 dat="/kyber/Data/Nanopore/Analysis/gmoney/CHM13/v1.0_final_assembly"
-chm13_meth <- read_tsv(paste0(dat, "/methylation_calls/methylation_frequency_50kb_split.tsv")) %>%
-  mutate(called_sites_unmethylated = called_sites - called_sites_methylated)
+chm13_meth <- import(paste0(dat, "/cut_and_run/210204_alignments/022021_CUTRUN/H3K27me3/CHM13_H3K27me3_cutnrun-202021_losalt.F3852.over.IlluminaPCRfree_v1.0-assembly_51mers_single_mrg_meryl_sort.bigwig"))
 
+# what type of repeat 
+type="SINE_HOTnot_"
 
 # load rep file
-reps <- read_tsv(paste0(dat, "/cut_and_run/H3K27me3_H3K4me2_mCpG_NovelgenesSkipZeros_sorted_clustered.bed")) %>%
+reps <- read_tsv(paste0(dat, "/TE/HOT_TE/Deeptools_beds/", type, "CHM13ABpool_htmpSorted.bed")) %>%
   dplyr::rename("chr"=`#chrom`) %>% 
+  filter(grepl("AluY",name)) %>%
   dplyr::rename("direction"=strand) %>%
   dplyr::select(c(chr, start, end, direction, deepTools_group)) %>%
   mutate(ID=row_number())
+
 # set flanks 
 flankn <- 3000
 bodylen <- 1
@@ -45,30 +49,32 @@ chm13.ovl <- as.data.frame(GRanges(chm13_meth)[queryHits(ovl),]) %>%
                               ifelse(dist < genewidth,
                                      bodylen * dist / genewidth,
                                      bodylen + (dist - genewidth)/flankn)), 
-                dist = round(dist,3)
+                dist = round(dist,1)
   )
+
 
 n_windows=500
 chm13.ovl$cut = cut(chm13.ovl$dist, breaks=n_windows)
 
 chm13.ovl.labs <- chm13.ovl %>%
-
-  group_by(cut) %>%
-  summarise(med = median(methylated_frequency), top = quantile(methylated_frequency, 0.75), bot = quantile(methylated_frequency, 0.25), n_genes = length(methylated_frequency),ID=ID) %>%
+  group_by(cut,deepTools_group) %>%
+  summarise(med = median(score), top = quantile(score, 0.75), bot = quantile(score, 0.25), n_genes = length(score)) %>%
   mutate(x_tmp = str_sub(cut, 2, -2)) %>% 
   separate(x_tmp, c("min", "max"), sep = ",") %>% 
   mutate_at(c("min", "max"), as.double) %>%
   ungroup() %>%
-  distinct() %>%
+  group_by(deepTools_group) %>%
   arrange(min) %>%
-  mutate(med_smooth = rollmean(med, 5, NA))
+  mutate(med_smooth = rollmean(med, 2, NA))
+
 
 # plot frequency with loess --- if doing SINEs don't use loess unless u want to die 
-freqplot <- ggplot(chm13.ovl.labs,aes(x=min,y=med))+theme(legend.position = "left", legend.direction="vertical",axis.title.x=element_blank())+ylim(0,1)+scale_x_continuous(breaks= c(-1,0,bodylen,bodylen + 1), labels = c(paste0("-",flankn/1e3,"kb"),"TTS","TES",paste0("+",flankn/1e3,"kb")))+geom_line()#+ geom_smooth(method="loess",se=F, span=.3)
+freqplot <- ggplot(chm13.ovl.labs,aes(x=min,y=med_smooth,color=deepTools_group))+theme(legend.position = "left", legend.direction="vertical",axis.title.x=element_blank())+scale_x_continuous(breaks= c(-1,0,bodylen,bodylen + 1), labels = c(paste0("-",flankn/1e3,"kb"),"TTS","TES",paste0("+",flankn/1e3,"kb")))+geom_smooth(method="loess", span=.2)
 
+# draw line where HOT elements end
 
 # plot heatmap
-plot <- ggplot(chm13.ovl,aes(x=dist,y=-ID,fill=methylated_frequency))+ geom_tile()+scale_fill_gradient(low = "blue", high = "red", na.value = NA, limits=c(0,1))+scale_x_continuous(breaks= c(-1,0,bodylen,bodylen + 1), labels = c(paste0("-",flankn/1e3,"kb"),"TTS","TES",paste0("+",flankn/1e3,"kb")))+theme(legend.position = "left", legend.direction="vertical")+theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(), axis.title.x=element_blank(),axis.title.y=element_blank())
+plot <- ggplot(chm13.ovl,aes(x=dist,y=-ID,fill=score))+ geom_tile()+scale_fill_gradient(low = "blue", high = "red", na.value = NA, limits=c(0,1))+scale_x_continuous(breaks= c(-1,0,bodylen,bodylen + 1), labels = c(paste0("-",flankn/1e3,"kb"),"TTS","TES",paste0("+",flankn/1e3,"kb")))+theme(legend.position = "left", legend.direction="vertical")+theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(), axis.title.x=element_blank(),axis.title.y=element_blank())
 
 # stack density and heatmap
 plot_grid(freqplot, plot, ncol=1, align = "v", rel_heights=c(1/3,1))
