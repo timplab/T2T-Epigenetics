ef91c6779ab4f5833722224f0e8fb774ac908092
gmoneyomics <agershm2@jhmi.edu>
2021-10-31 23:04:08 -0400

    HERVK

--- TE/DeeptoolsHeatmaps.R
+++ TE/DeeptoolsHeatmaps.R
@@ -18,14 +18,14 @@ chm13_meth <- read_tsv(paste0(dat, "/methylation_calls/methylation_frequency_50k
 
 
 # load rep file
-reps <- read_tsv(paste0(dat, "/cut_and_run/H3K27me3_H3K4me2_mCpG_NovelgenesSkipZeros_sorted_clustered.bed")) %>%
-  dplyr::rename("chr"=`#chrom`) %>% 
+reps <- read_tsv(paste0(dat, "/TE/revision/new_deeptools_beds/", "HERVK_byGroup_CHM13ABpool_anti-sense_htmpSorted.bed")) %>%
+  dplyr::rename("chr"=`#chrom`,"group"=deepTools_group) %>% 
   dplyr::rename("direction"=strand) %>%
-  dplyr::select(c(chr, start, end, direction, deepTools_group)) %>%
+  dplyr::select(c(chr, start, end,direction, group)) %>%
   mutate(ID=row_number())
 # set flanks 
-flankn <- 3000
-bodylen <- 1
+flankn <- 100
+bodylen <- 10
 
 # overlap with methylation calls and normalize by size 
 l1_regions <- reps %>%
@@ -40,7 +40,7 @@ genes.ovl <- as.data.frame(reps)[subjectHits(ovl),] %>%
 chm13.ovl <- as.data.frame(GRanges(chm13_meth)[queryHits(ovl),]) %>%
   bind_cols(genes.ovl) %>%
   dplyr::rename(seqnames = 1) %>%
-  dplyr::mutate(dist = ifelse(direction == "+",start - gene_start, gene_end - start),
+  dplyr::mutate(dist = ifelse(direction == "pos",start - gene_start, gene_end - start),
                 dist = ifelse(dist < 0, dist/flankn,
                               ifelse(dist < genewidth,
                                      bodylen * dist / genewidth,
@@ -53,28 +53,29 @@ chm13.ovl$cut = cut(chm13.ovl$dist, breaks=n_windows)
 
 chm13.ovl.labs <- chm13.ovl %>%
 
-  group_by(cut) %>%
+  group_by(cut,group) %>%
   summarise(med = median(methylated_frequency), top = quantile(methylated_frequency, 0.75), bot = quantile(methylated_frequency, 0.25), n_genes = length(methylated_frequency),ID=ID) %>%
   mutate(x_tmp = str_sub(cut, 2, -2)) %>% 
   separate(x_tmp, c("min", "max"), sep = ",") %>% 
   mutate_at(c("min", "max"), as.double) %>%
   ungroup() %>%
   distinct() %>%
+  group_by(group) %>%
   arrange(min) %>%
-  mutate(med_smooth = rollmean(med, 5, NA))
+  mutate(med_smooth = rollmean(med, 20, NA))
 
 # plot frequency with loess --- if doing SINEs don't use loess unless u want to die 
-freqplot <- ggplot(chm13.ovl.labs,aes(x=min,y=med))+theme(legend.position = "left", legend.direction="vertical",axis.title.x=element_blank())+ylim(0,1)+scale_x_continuous(breaks= c(-1,0,bodylen,bodylen + 1), labels = c(paste0("-",flankn/1e3,"kb"),"TTS","TES",paste0("+",flankn/1e3,"kb")))+geom_line()#+ geom_smooth(method="loess",se=F, span=.3)
-
+freqplot <- ggplot(chm13.ovl.labs,aes(x=min,y=med, color=group))+theme(legend.position = "left", legend.direction="vertical",axis.title.x=element_blank())+ylim(0,1)+scale_x_continuous(breaks= c(-1,0,bodylen,bodylen + 1), labels = c(paste0("-",flankn/1e3,"kb"),"TTS","TES",paste0("+",flankn/1e3,"kb")))+ geom_smooth(method="loess",se=F, span=.3)
+#+geom_line()#
 
 # plot heatmap
-plot <- ggplot(chm13.ovl,aes(x=dist,y=-ID,fill=methylated_frequency))+ geom_tile()+scale_fill_gradient(low = "blue", high = "red", na.value = NA, limits=c(0,1))+scale_x_continuous(breaks= c(-1,0,bodylen,bodylen + 1), labels = c(paste0("-",flankn/1e3,"kb"),"TTS","TES",paste0("+",flankn/1e3,"kb")))+theme(legend.position = "left", legend.direction="vertical")+theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(), axis.title.x=element_blank(),axis.title.y=element_blank())
+plot <- ggplot(chm13.ovl,aes(x=dist,y=-ID,fill=methylated_frequency))+ geom_tile()+scale_fill_gradient(low = "blue", high = "red", na.value = NA, limits=c(0,1))+scale_x_continuous(breaks= c(-1,0,bodylen,bodylen + 1), labels = c(paste0("-",flankn/1e3,"kb"),"TTS","TES",paste0("+",flankn/1e3,"kb")))+theme(legend.position = "left", legend.direction="vertical")+theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(), axis.title.x=element_blank(),axis.title.y=element_blank())+facet_wrap(~group, ncol=1)
 
 # stack density and heatmap
 plot_grid(freqplot, plot, ncol=1, align = "v", rel_heights=c(1/3,1))
 
 ggsave(
-  paste0(dat, "/figures/evol_meth/TE/",type, "_Heatmap.pdf"),
+  paste0(dat, "/figures/evol_meth/TE/HERVK_HeatmapRev_facetDeeptoolsGroup.pdf"),
   plot = last_plot(),
   scale = 1,
   width = 5,
